ceb197eb5b61d25ceed450b53f9bb544f9641851
JorisSittler <JorisSittler@users.noreply.github.com>
2019-03-22 23:40:40 +0100

    Structure calcul de performances théorique

---
+++ src/marche/donnees/courbes/Performance.java
@@ -0,0 +1,85 @@
+package marche.donnees.courbes;
+
+public abstract class Performance {
+
+	/**
+	 * @param vitesseAvant
+	 *            vitesse de début de phase
+	 * @param vitessePlafond
+	 *            vitesse de fin de phase
+	 * @return la distance parcourue en m à accélérer entre la vitesse a et la vitesse b
+	 */
+	public double distAcceleration(double vitesseAvant, double vitessePlafond) {
+		double dA = dAcc(vitesseAvant);
+		double dB = dAcc(vitessePlafond);
+		return dB - dA;
+	}
+
+	/**
+	 * @param vitesse
+	 *            la vitesse
+	 * @return la distance en m entre le démarrage et l'atteinte de la vitesse v
+	 */
+	public abstract double dAcc(double vitesse);
+
+	/**
+	 * @param vitesseA
+	 *            vitesse de début de phase
+	 * @param vitesseB
+	 *            vitesse de fin de phase
+	 * @return la distance parcourue en m à accélérer entre la vitesse a et la vitesse b
+	 */
+	public double distFreinage(double vitesseA, double vitesseB) {
+		double dA = dDec(vitesseA);
+		double dB = dDec(vitesseB);
+		return dA - dB;
+	}
+
+	/**
+	 * @param vitesse
+	 *            la vitesse
+	 * @return la distance parcourue en m entre le début du freinage en vitesse et l'arrêt
+	 */
+	public abstract double dDec(double vitesse);
+
+	/**
+	 * @param vitesseA
+	 *            vitesse de début de phase
+	 * @param vitesseB
+	 *            vitesse de fin de phase
+	 * @return le temps passé à accélérer entre la vitesse a et la vitesse b
+	 */
+	public double tempsAcceleration(double vitesseA, double vitesseB) {
+		double tA = tAcc(vitesseA);
+		double tB = tAcc(vitesseB);
+		return tB - tA;
+	}
+
+	/**
+	 * @param vitesse
+	 *            la vitesse
+	 * @return le temps en secondes entre le démarrage et l'atteinte de la vitesse v
+	 */
+	public abstract double tAcc(double vitesse);
+
+	/**
+	 * @param vitessePlafond
+	 *            vitesse de début de phase
+	 * @param vitesseApres
+	 *            vitesse de fin de phase
+	 * @return le temps passé à accélérer entre la vitesse a et la vitesse b
+	 */
+	public double tempsFreinage(double vitessePlafond, double vitesseApres) {
+		double tA = tDec(vitessePlafond);
+		double tB = tDec(vitesseApres);
+		return tA - tB;
+	}
+
+	/**
+	 * @param vitesse
+	 *            la vitesse
+	 * @return le temps en secondes entre le début du freinage en vitesse et l'arrêt
+	 */
+	public abstract double tDec(double vitesse);
+
+}
---
+++ src/marche/donnees/courbes/PerformanceCalculee.java
@@ -0,0 +1,49 @@
+package marche.donnees.courbes;
+
+public class PerformanceCalculee extends Performance {
+	protected double puissanceMassique;
+	protected double accMax;
+	/* décélération supposée constante */
+	protected double dec;
+
+	/**
+	 * en-dessous de cette vitesse l'accélération est plafonnée pour confort et/ou adhérence <br/>
+	 * au-dessus c'est la puissance qui limite l'accélération
+	 */
+	protected double vitessePMax() {
+		return 3.6 * puissanceMassique / accMax;
+	}
+
+	@Override
+	public double dAcc(double vitesse) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double dDec(double vitesse) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double tAcc(double vitesse) {
+		if (vitesse <= vitessePMax()) {
+			// on est uniquement dans la phase d'accélération constante
+			return vitesse / (3.6 * accMax);
+		} else {
+			// sinon on a d'abord le temps jusqu'à vitessePMAx
+			double tempsJusqueVPMax = vitessePMax() / (3.6 * accMax);
+
+			// auquel on ajoute le temps sur la partie à puissance constante, où l'accélération est hyperboloïde
+			// TODO
+		}
+		return 0;
+	}
+
+	@Override
+	public double tDec(double vitesse) {
+		// on est uniquement dans la phase d'accélération constante
+		return vitesse / (3.6 * dec);
+	}
+}
